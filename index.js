7a80866225ef9e6d20ced03371efb47d73008fb1
Jonathan T. Brooks <jonathan.brooks26@gmail.com>
2018-07-18 14:08:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(String) {
+    return String.toUpperCase() 
+}
+function whisper(String) {
+    return String.toLowerCase()
+}
+function logShout(String) {
+    console.log(String.toUpperCase()), 
+}
